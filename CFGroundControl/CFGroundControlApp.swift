49042ffcaa2ba89ee4cf7abf3452167b75c72a29
hadongz <mhadifaturrahman@gmail.com>
2025-07-25 16:23:27 +0700

    Initial Commit

---
+++ CFGroundControl/CFGroundControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFGroundControlApp.swift
+//  CFGroundControl
+//
+//  Created by Muhammad Hadi on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CFGroundControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
